13edafba71bcde8f67aaadad21e64f09eea4ccd7
rdininno <robertdininno@gmail.com>
2024-02-22 15:12:39 -0800

    add album component

---
+++ src/components/Album.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import ArrowButton from "./ArrowButton";
+import AudioPlayer from "./AudioPlayer";
+
+export default function Album({ albumName, tracks }) {
+  const [isAlbumListVisible, setIsAlbumListVisible] = useState(false);
+
+  const toggleAlbumListVisibility = () => {
+    setIsAlbumListVisible(!isAlbumListVisible);
+  };
+
+  return (
+    <div className="album">
+      <div className="album-header">
+        <ArrowButton
+          direction={isAlbumListVisible ? "down" : "right"}
+          onClick={toggleAlbumListVisibility}
+        />
+        <h2 className="album-name">{albumName}</h2>
+      </div>
+      {isAlbumListVisible && (
+        <div className="album-list">
+          {tracks.map((track, index) => (
+            <AudioPlayer key={index} src={track.src} title={track.title} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
