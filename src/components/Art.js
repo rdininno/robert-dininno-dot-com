e53eb8de8d88b61496791db4a366db66083b2a4d
rdininno <robertdininno@gmail.com>
2024-02-22 15:17:42 -0800

    add button functionality to art page

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,18 +1,28 @@
 import ArrowButton from "./ArrowButton";
-import AudioPlayer from "./AudioPlayer";
 
 export default function Art({ onMenuSelect }) {
   return (
     <div className="art-page">
-      <div className="art-content">
-        <div className="back-button">
-          <ArrowButton
-            direction="up"
-            onClick={() => onMenuSelect("MainPage")}
-          />
-
-          <div>
-            <AudioPlayer src="/audio/Festivals_OneMin.mp3" title="festivals" />
+      <div className="back-button">
+        <ArrowButton direction="up" onClick={() => onMenuSelect("MainPage")} />
+      </div>
+      <div>
+        <div className="art-content">
+          <div className="art-button-wrapper">
+            <button
+              className="art-page-button"
+              onClick={() => onMenuSelect("Music")}
+            >
+              Audio
+            </button>
+          </div>
+          <div className="art-button-wrapper">
+            <button
+              className="art-page-button"
+              onClick={() => onMenuSelect("Visual")}
+            >
+              Visual
+            </button>
           </div>
         </div>
       </div>
