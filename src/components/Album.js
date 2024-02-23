5413dfb6809affa0a57042afc1a7af9a7e72f927
rdininno <robertdininno@gmail.com>
2024-02-22 17:49:38 -0800

    update album component to control everything
    this upates the album component to control all
    state related to it. There was issues with playback using
    the audioplayer component and not registering the correct
    states. this puts everything in one place.

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,13 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import AlbumButton from "./AlbumButton";
-import AudioPlayer from "./AudioPlayer";
+import { PlayIcon, PauseIcon } from "@heroicons/react/24/solid"; // Assuming you're using Heroicons
 
 export default function Album({ albumName, tracks }) {
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
   const [isAlbumListVisible, setIsAlbumListVisible] = useState(false);
+  const audioRef = useRef(new Audio());
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const playTrack = (index) => {
+    const isNewTrack = index !== currentTrackIndex;
+    if (audioRef.current) {
+      if (isNewTrack) {
+        audioRef.current.src = tracks[index].src;
+        audioRef.current.play();
+        setCurrentTrackIndex(index);
+        setIsPlaying(true);
+      } else {
+        if (isPlaying) {
+          audioRef.current.pause();
+        } else {
+          audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+      }
+    }
+  };
 
   const toggleAlbumListVisibility = () => {
     setIsAlbumListVisible(!isAlbumListVisible);
+    if (isAlbumListVisible) {
+      setIsPlaying(false);
+      audioRef.current.pause();
+    }
   };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    const handleTrackEnd = () => {
+      const nextIndex =
+        currentTrackIndex + 1 < tracks.length ? currentTrackIndex + 1 : null;
+      if (nextIndex !== null) {
+        playTrack(nextIndex);
+      } else {
+        setCurrentTrackIndex(null);
+        setIsPlaying(false);
+      }
+    };
+
+    audio.addEventListener("ended", handleTrackEnd);
+    return () => {
+      audio.removeEventListener("ended", handleTrackEnd);
+    };
+  }, [currentTrackIndex, playTrack, tracks.length]);
 
   return (
     <div className="album">
@@ -18,7 +64,21 @@ export default function Album({ albumName, tracks }) {
       {isAlbumListVisible && (
         <div className="album-list">
           {tracks.map((track, index) => (
-            <AudioPlayer key={index} src={track.src} title={track.title} />
+            <div
+              key={index}
+              className={`track ${
+                index === currentTrackIndex ? "is-playing" : ""
+              }`}
+            >
+              <button className="audio-button" onClick={() => playTrack(index)}>
+                {currentTrackIndex === index && isPlaying ? (
+                  <PauseIcon className="play-pause-icon" />
+                ) : (
+                  <PlayIcon className="play-pause-icon" />
+                )}
+              </button>
+              <span className="track-title">{track.title}</span>
+            </div>
           ))}
         </div>
       )}
