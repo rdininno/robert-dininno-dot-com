f82937a2c816677972f5b5e6cc9f45310afde0d0
rdininno <robertdininno@gmail.com>
2024-02-28 14:08:04 -0800

    use heroicons for arrows button

--- src/components/ArrowButton.js
+++ src/components/ArrowButton.js
@@ -1,10 +1,31 @@
+import {
+  ArrowUpIcon,
+  ArrowRightIcon,
+  ArrowDownIcon,
+  ArrowLeftIcon,
+} from "@heroicons/react/24/outline";
+
 export default function ArrowButton({ direction = "right", onClick }) {
+  const iconProps = { className: "h-6 w-6", "aria-hidden": "true" };
+
+  const renderIcon = () => {
+    switch (direction) {
+      case "up":
+        return <ArrowUpIcon {...iconProps} />;
+      case "right":
+        return <ArrowRightIcon {...iconProps} />;
+      case "down":
+        return <ArrowDownIcon {...iconProps} />;
+      case "left":
+        return <ArrowLeftIcon {...iconProps} />;
+      default:
+        return null; // or a default icon
+    }
+  };
+
   return (
     <button className={`arrowButton ${direction}`} onClick={onClick}>
-      {direction === "up" && "↑"}
-      {direction === "right" && "→"}
-      {direction === "down" && "↓"}
-      {direction === "left" && "←"}
+      {renderIcon()}
     </button>
   );
 }
