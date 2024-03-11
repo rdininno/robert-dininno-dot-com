0cd01a9a0034ccb2545c41923cd661e475f1498c
rdininno <robertdininno@gmail.com>
2024-03-11 12:51:35 -0700

    update arrowbutton to use svg

--- src/components/ArrowButton.js
+++ src/components/ArrowButton.js
@@ -1,21 +1,74 @@
-import {
-  ArrowUpIcon,
-  ArrowRightIcon,
-  ArrowDownIcon,
-  ArrowLeftIcon,
-} from "@heroicons/react/24/outline";
-
 export default function ArrowButton({ direction = "right", onClick }) {
   const renderIcon = () => {
     switch (direction) {
       case "up":
-        return <ArrowUpIcon />;
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5 10l7-7m0 0l7 7m-7-7v18"
+            />
+          </svg>
+        );
       case "right":
-        return <ArrowRightIcon />;
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            />
+          </svg>
+        );
       case "down":
-        return <ArrowDownIcon />;
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19 14l-7 7m0 0l-7-7m7 7V3"
+            />
+          </svg>
+        );
       case "left":
-        return <ArrowLeftIcon />;
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+        );
       default:
         return null;
     }
